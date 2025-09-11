7ad27318ae8a9cc4b7038509ef289a1712bcaf43
Lamat Abdellahi <lamat032025@gmail.com>
2025-09-11 23:37:05 +0100

    add pages

--- .history/app/api/inngest/route_20250911233657.js
+++ .history/app/api/inngest/route_20250911233657.js
@@ -5,5 +5,5 @@ import { serve } from "inngest/next";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [syncUserCreation, syncUserUpdate, syncUserDelete],
-  verifySignature: false, 
+  
 });
