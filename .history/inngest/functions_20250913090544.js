6ff91eaba2817ac19e46e634f0c5f6abded8ff01
Lamat Abdellahi <lamat032025@gmail.com>
2025-09-13 09:07:34 +0100

    add pages

--- .history/inngest/functions_20250913090544.js
+++ .history/inngest/functions_20250913090544.js
@@ -13,7 +13,8 @@ export const syncUserCreation = inngest.createFunction(
     await prisma.user.create({
       data: {
         id: data.id,
-        email: data.email_addresses[0].email_address,
+       email: data.email_addresses[0].email_address,
+
         name: `${data.first_name} ${data.last_name}`,
         image: data.image_url,
       },
