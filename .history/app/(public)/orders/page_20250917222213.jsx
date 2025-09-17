08d24edb3823756150eec87646d5949c56f0d604
Lamat Abdellahi <lamat032025@gmail.com>
2025-09-17 22:46:36 +0100

    add pages

---
+++ .history/app/(public)/orders/page_20250917222213.jsx
@@ -0,0 +1,70 @@
+"use client";
+import PageTitle from "@/components/PageTitle";
+import { useEffect, useState } from "react";
+import OrderItem from "@/components/OrderItem";
+import { orderDummyData } from "@/assets/assets";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
+
+export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const { getToken } = useAuth();
+const {user} =  useUser()
+  useEffect(() => {
+
+    const fetchOrders = async () => {
+      try {
+        const token = await getToken();
+        const { data } = await axios.get("/api/orders", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setOrders(data.orders || []);
+      } catch (error) {
+        console.error(
+          "❌ Error fetching orders:",
+          error.response?.data || error.message
+        );
+      }
+    };
+
+    fetchOrders();
+  }, [getToken]);
+
+  return (
+    <div className="min-h-[70vh] mx-6">
+      {orders.length > 0 ? (
+        <div className="my-20 max-w-7xl mx-auto">
+          <PageTitle
+            heading="My Orders"
+            text={`Showing total ${orders.length} orders`}
+            linkText={"Go to home"}
+          />
+
+          <table className="w-full max-w-5xl text-slate-500 table-auto border-separate border-spacing-y-12 border-spacing-x-4">
+            <thead>
+              <tr className="max-sm:text-sm text-slate-600 max-md:hidden">
+                <th className="text-left">Product</th>
+                <th className="text-center">Total Price</th>
+                <th className="text-left">Address</th>
+                <th className="text-left">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <OrderItem order={order} key={order.id} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
+          <h1 className="text-2xl sm:text-4xl font-semibold">
+            You have no orders
+          </h1>
+        </div>
+      )}
+    </div>
+  );
+}
