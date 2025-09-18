e4b7c398f8a768173d6545c22c38e85d19fafbdf
Lamat Abdellahi <lamat032025@gmail.com>
2025-09-18 21:49:02 +0100

    add pages

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -4,66 +4,60 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function POST(req) {
+export const POST = async (req) => {
   try {
     const body = await req.text();
-    const sig = req.get("stripe-signature");
+    const sig = req.headers.get("stripe-signature");
+
     const event = stripe.webhooks.constructEvent(
       body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
-    // Fonction pour gérer le PaymentIntent
+
     const handlePaymentIntent = async (paymentIntentId, isPaid) => {
       const session = await stripe.checkout.sessions.list({
         payment_intent: paymentIntentId,
       });
 
       const { orderIds, userId, appId } = session.data[0].metadata;
-      if (appId !== "RimCart") {
-       return NextResponse.json({ received: true, message: "Invalid app id" });
 
+      if (appId !== "RimCart") {
+        return NextResponse.json({ received: true, message: "Invalid app id" });
       }
+
       const orderIdsArray = orderIds.split(",");
+
       if (isPaid) {
         await Promise.all(
-          orderIdsArray.map(async (orderId) => {
-            await prisma.order.update({
+          orderIdsArray.map(async (orderId) =>
+            prisma.order.update({
               where: { id: orderId },
-              data: {
-                isPaid: true,
-              },
-            });
-          })
+              data: { isPaid: true },
+            })
+          )
         );
-        //clear cart
+
         await prisma.user.update({
           where: { id: userId },
-          data: {
-            cart: {},
-          },
+          data: { cart: {} },
         });
       } else {
         await Promise.all(
-          orderIdsArray.map(async (orderId) => {
-            await prisma.order.delete({
-              where: { id: orderId }
-            });
-          })
+          orderIdsArray.map(async (orderId) =>
+            prisma.order.delete({ where: { id: orderId } })
+          )
         );
       }
     };
-    // Gérer les événements Stripe
+
     switch (event.type) {
-      case "payment_intent.succeeded": {
+      case "payment_intent.succeeded":
         await handlePaymentIntent(event.data.object.id, true);
         break;
-      }
-
-      case "payment_intent.canceled": {
+      case "payment_intent.canceled":
         await handlePaymentIntent(event.data.object.id, false);
         break;
-      }
       default:
         console.log(`Unhandled event type: ${event.type}`);
         break;
@@ -73,8 +67,8 @@ export default async function POST(req) {
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
-}
+};
 
-export const config ={
-    api:{bodyparser:false}
-}
+export const config = {
+  api: { bodyParser: false },
+};
